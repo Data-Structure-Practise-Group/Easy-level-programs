914a7be978025b7e4b880ae4330ac29f517c33f5
Deepali Takyar <takyar.deepali30@gmail.com>
2020-10-05 01:53:26 +0530

    pattern2

---
+++ pattern2.cpp
@@ -0,0 +1,46 @@
+/*   11
+                                        *
+                                *       *       *
+                        *       *       *       *       *
+                *       *       *       *       *       *       *
+        *       *       *       *       *       *       *       *       *
+*       *       *       *       *       *       *       *       *       *       *
+        *       *       *       *       *       *       *       *       *
+                *       *       *       *       *       *       *
+                        *       *       *       *       *
+                                *       *       *
+                                        *
+
+*/
+//number should be odd
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;  
+    
+    if(n>=1 && n<=100 && (n%2!=0) ) {
+        for(int i=0; i<=n/2; i++){   
+            for(int k=n/2; k>i; k--) 
+                cout<<"\t";
+            for(int j=0; j<=2*i-1 ; j++) 
+                cout<<"*\t";
+            cout<<"*\t"<<endl;
+        }
+        
+        for(int i=n/2-1; i>=0; i--){   
+            for(int k=n/2; k>i; k--) 
+                cout<<"\t";
+            for(int j=2*i-1; j>=0 ; j--) 
+                cout<<"*\t";
+            cout<<"*\t"<<endl;
+        }
+    }
+    
+    
+    return 0;
+}
+
+
